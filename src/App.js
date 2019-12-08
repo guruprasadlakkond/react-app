973645879c98b9214f64beb71d9c6e1f476662f6
guruprasadlakkond <guruprasadlakkond@gmail.com>
2019-12-09 01:13:44 +0530

    Adding comment section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import faker from 'faker';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
         </a>
-        <div>
-          <label class="label" for="name">
-            Enter name:
-          </label>
-          <input id="name"type="text"/>
-          <button style={{backgroundColor: "white", color:"white"}}>Submit</button>
+        <div className="content">
+          <a href="/" className="comment">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice blog post</div>
         </div>
-      </header>
+      </div>
     </div>
-  );
+  )
 }
+
+/* <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+    <div>
+      <label className="Label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name"type="text"/>
+      <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
+    </div>
+  </header>
+</div> */
 
 export default App;
